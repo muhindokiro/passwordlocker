faa63993c3ba5db8894a0f687f215176b3bcd2f8
muhindokiro <49603911+muhindokiro@users.noreply.github.com>
2019-06-17 09:09:59 +0300

    add function to allow users to input their details

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def find_account(text):
 
 def check_existing_accounts(text):
     '''
-    Function that check if an account exists wit that platform and returns a Boolean
+    Function that check if an account exists wi that platform and returns a Boolean
     '''
     return User.account_exist(text)
 
